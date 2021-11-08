9ceea369e6414c81282589eeb53539aa7e6dabdf
stoyanov7 <georgi.stoyanov777@gmail.com>
2021-11-08 16:21:25 +0200

    initial project, add endpoints

---
+++ src/FeshShop.ApiGateway/Program.cs
@@ -0,0 +1,22 @@
+namespace FeshShop.ApiGateway
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+            => Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("ocelot.json")
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .Build()
+                .Run();
+    }
+}
---
+++ src/FeshShop.ApiGateway/Startup.cs
@@ -0,0 +1,31 @@
+namespace FeshShop.ApiGateway
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Ocelot.DependencyInjection;
+    using Ocelot.Middleware;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => this.Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) => services.AddOcelot(this.Configuration);
+
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+            await app.UseOcelot();
+        }
+    }
+}
